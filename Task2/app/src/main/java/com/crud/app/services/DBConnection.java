ea54d0dadb28022d59a17e26ada6698962e23334
Nikita Gill <gilnikita026@gmail.com>
2024-02-21 00:44:09 +0300

    update branch main

--- Task2/app/src/main/java/com/crud/app/services/DBConnection.java
+++
@@ -1,9 +0,0 @@
-package com.crud.app.services;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface DBConnection {
-	public Connection openConnection() throws SQLException;
-	public void closeConnection(Connection connection) throws SQLException;
-}
