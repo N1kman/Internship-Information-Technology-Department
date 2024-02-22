2c877af0943727e7bdaf1fc41bd077b2a35cfed1
Nikita Gill <gilnikita026@gmail.com>
2024-02-22 18:03:39 +0300

    added log4j2 Logger

--- src/main/java/com/crud/app/App.java
+++ src/main/java/com/crud/app/App.java
@@ -1,10 +1,32 @@
 package com.crud.app;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.crud.app.entities.Aircraft;
+import com.crud.app.services.impl.ServiceDBConnection;
+
 public class App 
 {
+	public static final Logger logger = LoggerFactory.getLogger(App.class.getName());
     public static void main( String[] args )
     {
+    	Aircraft aircraft = new Aircraft(1l, "777", "air", "Belavia");
+    	logger.trace("Creates aircraft {}", aircraft);
+    	ServiceDBConnection serviceDBConnection = new ServiceDBConnection();
     	
-    	System.out.print("Hello");
+    	try {
+    		serviceDBConnection.setLocalDriver();
+    		Connection connection = serviceDBConnection.openConnection();
+    		serviceDBConnection.closeConnection(connection);
+    	} catch(SQLException exception) {
+    		logger.error("Problems with SQL connection. Status: {}", exception.getErrorCode());
+    	} catch(ClassNotFoundException exception) {
+    		logger.error("Problems setting LocalDriver. Message: {}", exception.getMessage());
+    	}
+    	
     }
 }
--- src/main/java/com/crud/app/services/impl/ServiceProps.java
+++ src/main/java/com/crud/app/services/impl/ServiceProps.java
@@ -2,8 +2,11 @@ package com.crud.app.services.impl;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ServiceProps {
+	public static final Logger logger = LoggerFactory.getLogger(ServiceProps.class.getName());
 	String filename;
 	
 	public ServiceProps(String filename) {
@@ -13,9 +16,10 @@ public class ServiceProps {
 	public PropertiesConfiguration loadProps() {
 		PropertiesConfiguration props = null;
 		try {
+			logger.trace("Creates and loads the extended properties from \"{}\"", filename);
 			props = new PropertiesConfiguration(filename);
 		}  catch(ConfigurationException exception) {
-			System.err.println(exception.getMessage());
+			logger.error("Problems reading the extended properties from \"{}\"", filename);
 		}
 		return props;	
 	}
