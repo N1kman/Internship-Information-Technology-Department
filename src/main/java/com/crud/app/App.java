0703d69ecccba2ce5acfc5da1ddffd9714bbcc84
Nikita Gill <gilnikita026@gmail.com>
2024-03-11 23:51:56 +0300

    started 3-rd task

--- src/main/java/com/crud/app/App.java
+++ src/main/java/com/crud/app/App.java
@@ -1,24 +1,11 @@
 package com.crud.app;
 
-import com.crud.app.entities.Flight;
-import com.crud.app.exceptions.DAOException;
-import com.crud.app.services.DAO;
-import com.crud.app.services.impl.FlightDAOImpl;
-import com.crud.app.services.impl.ServiceConnectionFactory;
-
 public class App 
 {
     public static void main(String[] args)
     {
     	
-    	DAO<Flight> dao = new FlightDAOImpl();
-    	try {
-    		ServiceConnectionFactory.setLocalDriverFromProperties();	
-			System.out.println(dao.findAll());
 
-		} catch (DAOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
     	
     }
 }
